1b215830adfbe1c0d7576fbfdb09b34c192bcf12
220-24\fuzzydice <brainjisu@gmail.com>
2025-07-10 09:57:15 +0900

    컨트롤러 테스트 완료

--- src/test/java/org/zerock/persistence/DataSourceTests.java
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -26,21 +26,22 @@ public class DataSourceTests {
 
 	@Setter(onMethod_ = @Autowired) // 세터 자동생성 setDataSource(DataSource)
 	private DataSource dataSource;
-	
-	@Setter(onMethod_=@Autowired)
+
+	@Setter(onMethod_ = @Autowired)
 	private SqlSessionFactory sqlSessionFactory;
-	
+
 	@Test
 	public void testMyBatis() {
-		try(SqlSession session = sqlSessionFactory.openSession();
-				Connection con = session.getConnection(); // 마이 바티스 연결용 코드
-				){
+
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection(); // 마이바티스
+		) {
 			log.info(session);
 			log.info(con);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			fail(e.getMessage());
 		}
-				
+
 	}
 
 	@Test
